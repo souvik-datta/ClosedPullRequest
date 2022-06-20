eae7c65aa6f60cf0120889a55d06ab89db0e1ed4
souvik <souvikdatta900@gmail.com>
2022-06-20 05:33:16 +0530

    Comment Added.

--- app/src/main/java/com/souvik/naviapp/constants/Constants.kt
+++ app/src/main/java/com/souvik/naviapp/constants/Constants.kt
@@ -1,5 +1,11 @@
 package com.souvik.naviapp.constants
 
 object Constants {
+    /*
+        I have used Mr Torvalds git repo to do this project
+        my public projects do not have any PR raised
+
+        My git id: souvik-datta
+    */
     const val REPO_OWNER_NAME = "torvalds"
 }
